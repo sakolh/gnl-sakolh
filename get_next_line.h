1240a22e003589493ebb58990578d0eec15f67b7
Sakol Hiraprayurpong <shirapra@z3t1c2.42bangkok.com>
2022-03-20 02:26:05 +0700

    update

--- get_next_line.h
+++ get_next_line.h
@@ -6,8 +6,16 @@
 /*   By: shirapra <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/19 22:34:48 by shirapra          #+#    #+#             */
-/*   Updated: 2022/03/19 22:34:53 by shirapra         ###   ########.fr       */
+/*   Updated: 2022/03/20 02:25:03 by shirapra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#ifndef	GET_NEXT_LINE_H
+#	define	GET_NEXT_LINE_H
+
+#	include	<stdio.h>
+#	include <stdlib.h>
+
+#	include <sys/types.h>
+#	include <sys/uioh>
 
 
