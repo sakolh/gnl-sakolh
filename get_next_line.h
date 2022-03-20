dd81192d2a1bab68a4b78fef1046017e01a68811
Sakol Hiraprayurpong <shirapra@z3t1c1.42bangkok.com>
2022-03-20 23:24:26 +0700

    gnlh-update

--- get_next_line.h
+++ get_next_line.h
@@ -6,16 +6,22 @@
 /*   By: shirapra <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/19 22:34:48 by shirapra          #+#    #+#             */
-/*   Updated: 2022/03/20 02:25:03 by shirapra         ###   ########.fr       */
+/*   Updated: 2022/03/20 23:21:14 by shirapra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef	GET_NEXT_LINE_H
 #	define	GET_NEXT_LINE_H
+#	define	BUFF_SIZE 8
+#	define	FD_SIZE 4096
 
 #	include	<stdio.h>
 #	include <stdlib.h>
-
+#	include	<fcnltl.h>
 #	include <sys/types.h>
-#	include <sys/uioh>
+#       include <sys/uioh>
+#	include	<unistd.h>
+#	include	"libft/libft.h"
 
+int	get_enxt_line(const int fd, char **line);
 
+#endif
