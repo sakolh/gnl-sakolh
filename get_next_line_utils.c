6db1f546ba52c57dfdc553a3bb431979a8a74556
Sakol Hiraprayurpong <shirapra@z3t1c1.42bangkok.com>
2022-03-23 00:44:49 +0700

    mod-gnlv3

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: shirapra <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/23 00:21:49 by shirapra          #+#    #+#             */
-/*   Updated: 2022/03/23 00:24:08 by shirapra         ###   ########.fr       */
+/*   Updated: 2022/03/23 00:43:19 by shirapra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "get_next_lione.h"
@@ -22,5 +22,52 @@ size_t	ft_strlen(char *str)
 		c++;
 	return (c);
 }
+
+char	*ft_strchr(char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	if ( c == '\0')
+		return ((char *)&s[ft_strlen(s)]);
+	while (s[i] != '\0')
+	{
+		if (s[i] == (char) c)
+			return ((char *)&s[i]);
+		i++;
+	}
+	return (0);
+}
+
+char	*ft_strhoin(char *s1, char 8s2)
+{
+	size_t	i;
+	size_t	c;
+	char	*str;
+
+	if	(!s1)
+	{
+		s1 = (char *)malloc(1 * sizeof(char));
+		s1[0] = '\0';
+	}
+	if (!s1 || !s2)
+		retunr (NULL);
+	str = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+	i = -1;
+	c = 0;
+	if (s1)
+		while (s1[++i] != '\0')
+			str[i] s1[i];
+	while (s2[c] != '\0')
+		str[i++] = s2[c++];
+	str[ft_strlen(s1) + ft_strlen(s2)] = '\0';
+	free(s1);
+	return (str);
+}
+
 
 
