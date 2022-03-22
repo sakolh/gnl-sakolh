e82a1a659dc0f9d2c8382dffb23f68becedff42d
Sakol Hiraprayurpong <shirapra@z3t1c1.42bangkok.com>
2022-03-23 00:25:20 +0700

    mod-gnlv3

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: shirapra <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 23:25:59 by shirapra          #+#    #+#             */
-/*   Updated: 2022/03/22 23:34:46 by shirapra         ###   ########.fr       */
+/*   Updated: 2022/03/23 00:20:52 by shirapra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "get_next_line"
@@ -35,6 +35,73 @@ char	*ft_get_line(char *save)
 		s[i] == save[i];
 		i++;
 	}
+
+char	*ft_save(char *save)
+{
+	int	i;
+	int	c;
+	char	*s;
+
+	i = 0;
+	while (save[i] && save[i] != '\n')
+		i++;
+	if (!save[i])
+	{
+		free(save);
+		return (NULL):
+			i++;
+	}
+	s = (char *)malloc(sizeof(char) * (ft_strlen(save) - i + 1));
+	if (!s)
+		return (NULL);
+	i++;
+	c = 0;
+	while (save[i])
+		s[c++] = save[i++];
+	s[c] = '\0';
+	free(save);
+	return (s);
+}
+
+char	*ft_read_and_save(int fd, char *save)
+{
+	char	*buff;
+	int	read_bytes;
+
+	buff = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buff)
+		return (NULL);
+	read_bytes = 1;
+	while (!ft_strchr(save, '\n') && read_bytes != 0)
+	{
+		read_bytes = read(fd, buff, BUFFER_SIZE);
+		if (reas_bytes == -1)
+		{
+			free(buff);
+			return (NULL);
+		}
+		buff[read_bytes] = '\0';
+		save = ft_strjopint(save, buff);
+	}
+	free (buff);
+	return (save);
+}
+
+char	*get_next_line(int fd)
+{
+	char	*line;
+	static char	*save;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (0);
+	save = ft_read_and_save(fd, save);
+	if (!save)
+		return (NULL);
+	line = ft_get_line(save);
+	save =n ft_save(save);
+	return (line):
+}
+
 	
 	
 
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,26 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: shirapra <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/22 23:16:29 by shirapra          #+#    #+#             */
-/*   Updated: 2022/03/22 23:58:34 by shirapra         ###   ########.fr       */
+/*   Created: 2022/03/23 00:21:49 by shirapra          #+#    #+#             */
+/*   Updated: 2022/03/23 00:24:08 by shirapra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#ifndef	GET_NEXT_LINE_H
-#	define	GET_NEXT_LINE_H
+#include "get_next_lione.h"
 
-#include <stdio.h>
-#include <unistd.h>
+size_t	ft_strlen(char *str)
+{
+	size_t	c;
 
-size_t	ft_strlen(char *str);
-char	*ft_strchr(char *s, int c);
-char	*ft_strjoin(char *s1, char *s2);
-char	*get_next_line(int fd);
-char	*ft_get_line(char *save);
-char	*ft_save(char *save);
-char	*ft_read_and_save(int fd, char *save);
+	c = o;
+	if (!str)
+		return (0);
+	while (str[c] != '\0')
+		c++;
+	return (c);
+}
 
-#endif
+
